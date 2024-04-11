0459badeaf40c41a25108627c043d642b6960bf7
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-04-10 23:33:58 -1200

    Improvement Calculate Ui

--- app/src/main/java/com/example/coinmarket/ui/feature/CalculatorScreen.kt
+++ app/src/main/java/com/example/coinmarket/ui/feature/CalculatorScreen.kt
@@ -3,13 +3,14 @@ package com.example.coinmarket.ui.feature
 import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,6 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -97,8 +100,11 @@ fun CryptoNumber(coinPrice: Float) {
     Row(
         Modifier
             .fillMaxWidth()
-            .border(1.dp , Color.Black)
-            .padding(24.dp),
+            .height(150.dp)
+            .padding(horizontal = 24.dp)
+            .shadow(4.dp)
+            .padding(12.dp)
+            .clip(RoundedCornerShape(100.dp)),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
