2d8a783d5ccecafb29fd4c388eaa2a3c6ceb6cff
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-04-21 18:24:16 -1200

    Create Toolbar Setting Screen

--- app/src/main/java/com/example/coinmarket/ui/feature/SettingScreen.kt
+++ app/src/main/java/com/example/coinmarket/ui/feature/SettingScreen.kt
@@ -1,12 +1,66 @@
 package com.example.coinmarket.ui.feature
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.coinmarket.ui.theme.TextBlack
 
 @Composable
 fun SettingScreen(navController: NavHostController) {
 
+    Column(
+        Modifier.fillMaxSize()
+            .background(Color.White)
+    ) {
 
+        SettingToolbar {
+            navController.popBackStack()
+        }
+
+
+    }
+
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingToolbar(onBackClicked :() -> Unit){
+
+    TopAppBar(
+        title = { Text(
+            text = "Setting",
+            style = TextStyle(
+                color = TextBlack,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(start = 8.dp)
+        ) },
+        navigationIcon = {
+            IconButton(onClick = { onBackClicked.invoke() }) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = null)
+            }
+        }
+    )
 
 
 }
