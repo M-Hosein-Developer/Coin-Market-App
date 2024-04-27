13f0bb3d9615adb95bc58a225493553500b6efac
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-04-26 23:35:29 -1200

    Setup Bookmark Ui List And get Bookmark Data From Db

--- app/src/main/java/com/example/coinmarket/ui/feature/BookmarkScreen.kt
+++ app/src/main/java/com/example/coinmarket/ui/feature/BookmarkScreen.kt
@@ -1,8 +1,14 @@
 package com.example.coinmarket.ui.feature
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,17 +17,39 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.coinmarket.R
+import com.example.coinmarket.model.dataClass.BookmarkResponse
+import com.example.coinmarket.ui.theme.Green
+import com.example.coinmarket.ui.theme.Red
+import com.example.coinmarket.util.MyScreens
+import com.example.coinmarket.util.chartUrl
+import com.example.coinmarket.util.imageUrl
 import com.example.coinmarket.viewModel.MainViewModel
 
 @Composable
 fun BookmarkScreen(navController: NavHostController, viewModel: MainViewModel) {
+
+    val getBookmarkData by remember { mutableStateOf(viewModel.getCryptoBookmarkList.value) }
 
     Column(
         Modifier.fillMaxSize()
@@ -29,6 +57,11 @@ fun BookmarkScreen(navController: NavHostController, viewModel: MainViewModel) {
 
         BookmarkToolbar{ navController.popBackStack() }
 
+        CryptoBookmarkList(
+            getBookmarkData
+        ) {
+            navController.navigate(MyScreens.DetailScreen.route + "/" + it)
+        }
 
     }
 
@@ -57,3 +90,144 @@ fun BookmarkToolbar(onBackPress :() -> Unit){
 
 
 }
+
+@Composable
+fun CryptoBookmarkList(getCoinList: List<BookmarkResponse.Data.CryptoCurrency>, onClickedItem: (Int) -> Unit) {
+
+    if (getCoinList.size >= 1) {
+
+        LazyColumn(
+
+            Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            items(getCoinList.size) {
+                CryptoBookmarkListItem(getCoinList[it], onClickedItem)
+            }
+        }
+
+    } else {
+
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            BookmarkLoading()
+            Text(text = "There is no much crypto in the list")
+        }
+
+    }
+
+
+}
+
+@Composable
+fun CryptoBookmarkListItem(coin: BookmarkResponse.Data.CryptoCurrency, onClickedItem: (Int) -> Unit) {
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClickedItem.invoke(coin.id) }
+    ) {
+
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl(coin.id))
+                .decoderFactory(SvgDecoder.Factory())
+                .build(), contentDescription = null,
+            modifier = Modifier.size(50.dp)
+        )
+
+        Column(
+            Modifier.padding(start = 8.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.Start
+        ) {
+
+            Text(
+                text = coin.name,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            if (coin.quotes[0].percentChange24h > 0) {
+                Text(
+                    text = "%+" + coin.quotes[0].percentChange24h.toString().subSequence(0, 4),
+                    color = Green
+                )
+            } else if (coin.quotes[0].percentChange24h < 0) {
+                Text(
+                    text = "%" + coin.quotes[0].percentChange24h.toString().subSequence(0, 4),
+                    color = Red
+                )
+            } else {
+                Text(
+                    text = "%" + coin.quotes[0].percentChange24h.toString().subSequence(0, 4))
+            }
+
+        }
+
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(chartUrl(coin.id))
+                .decoderFactory(SvgDecoder.Factory())
+                .build(), contentDescription = null,
+            modifier = Modifier.size(120.dp),
+            colorFilter =
+            if (coin.quotes[0].percentChange24h > 0) {
+                ColorFilter.tint(Green)
+            } else {
+                ColorFilter.tint(Red)
+            }
+        )
+
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+
+            Text(
+                text = "$" + (coin.quotes[0].price.toString()).subSequence(0, 7),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = coin.ath.toString().subSequence(0, 7).toString() + " " + coin.symbol,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                )
+            )
+
+
+        }
+
+    }
+
+}
+
+//loading animation
+@Composable
+fun BookmarkLoading() {
+
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.loading_anime)
+    )
+
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever,
+        modifier = Modifier.size(150.dp),
+    )
+}
+
