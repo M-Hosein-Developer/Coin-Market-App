1c30fb6b109e630dcdb1e416d6ba2df68b1c7125
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-09-29 03:00:27 -1200

    Add Dependency Between To Module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.1")
+    implementation(project(":data"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.5")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- presentation/src/main/java/com/example/coinmarket/di/module/DatabaseModule.kt
+++ presentation/src/main/java/com/example/coinmarket/di/module/DatabaseModule.kt
@@ -2,13 +2,13 @@ package com.example.coinmarket.di.module
 
 import android.content.Context
 import androidx.room.Room
-import ir.androidcoder.local.MyDatabase
-import ir.androidcoder.local.RoomDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import ir.androidcoder.local.MyDatabase
+import ir.androidcoder.local.RoomDao
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -18,15 +18,15 @@ object DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context) : ir.androidcoder.local.MyDatabase {
-        return Room.databaseBuilder(context , ir.androidcoder.local.MyDatabase::class.java , "databse.db")
+    fun provideDatabase(@ApplicationContext context: Context) : MyDatabase {
+        return Room.databaseBuilder(context , MyDatabase::class.java , "databse.db")
             .fallbackToDestructiveMigration()
             .build()
     }
 
     @Singleton
     @Provides
-    fun provideDao(database: ir.androidcoder.local.MyDatabase) : ir.androidcoder.local.RoomDao {
+    fun provideDao(database: MyDatabase) : RoomDao {
         return database.roomDao()
     }
 
