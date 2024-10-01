9a381eaba96c350a1c1df13b5362768ad86482b0
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-10-01 02:40:14 -1200

    Setup Theme Repository

---
+++ data/src/main/java/ir/androidcoder/repositoriesImpl/ThemeRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package ir.androidcoder.repositoriesImpl
+
+import ir.androidcoder.entities.DynamicThemeEntity
+import ir.androidcoder.mapper.toThemeEntity
+import ir.androidcoder.mapper.toThemeModel
+import ir.androidcoder.repositories.themeRepo.ThemeRepository
+
+
+class ThemeRepositoryImpl (private val myDao: ir.androidcoder.local.RoomDao) : ThemeRepository {
+
+
+    override suspend fun insertDynamicThemeStateRep(state: DynamicThemeEntity) = myDao.insertDynamicTheme(state.toThemeModel())
+
+    override suspend fun getDynamicThemeState(): DynamicThemeEntity = myDao.getDynamicThemeState().toThemeEntity()
+
+
+}
