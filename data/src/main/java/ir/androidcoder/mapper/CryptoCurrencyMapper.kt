0a6cd692682724d9108bd93ca11771b9bbefbaba
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-10-01 01:58:45 -1200

    Setup Price And Crypto Mapper

--- data/src/main/java/ir/androidcoder/mapper/CryptoCurrencyMapper.kt
+++ data/src/main/java/ir/androidcoder/mapper/CryptoCurrencyMapper.kt
@@ -131,3 +131,50 @@ fun CryptoCurrencyEntity.AuditInfo.toCryptoModel(): CoinMarketResponse.Data.Cryp
     )
 
 //--------------------------------------------------------------------------------------------------
+
+fun BookmarkResponse.Data.CryptoCurrency.toBookmarkEntity(): CryptoCurrencyEntity = CryptoCurrencyEntity(
+        ath = this.ath,
+        atl = this.atl,
+        badges = this.badges,
+        circulatingSupply = this.circulatingSupply,
+        cmcRank = this.cmcRank,
+        dateAdded = this.dateAdded,
+        high24h = this.high24h,
+        id = this.id,
+        isActive = this.isActive,
+        isAudited = this.isAudited,
+        lastUpdated = this.lastUpdated,
+        low24h = this.low24h,
+        marketPairCount = this.marketPairCount,
+        maxSupply = this.maxSupply,
+        name = this.name,
+        quotes = this.quotes.map { it.toBookmarkEntity() },  // Map list of quotes
+        selfReportedCirculatingSupply = this.selfReportedCirculatingSupply,
+        slug = this.slug,
+        symbol = this.symbol,
+        totalSupply = this.totalSupply
+)
+
+fun BookmarkResponse.Data.CryptoCurrency.Quote.toBookmarkEntity(): CryptoCurrencyEntity.Quote = CryptoCurrencyEntity.Quote(
+        dominance = this.dominance,
+        fullyDilluttedMarketCap = this.fullyDilluttedMarketCap,
+        lastUpdated = this.lastUpdated,
+        marketCap = this.marketCap,
+        marketCapByTotalSupply = this.marketCapByTotalSupply,
+        name = this.name,
+        percentChange1h = this.percentChange1h,
+        percentChange1y = this.percentChange1y,
+        percentChange24h = this.percentChange24h,
+        percentChange30d = this.percentChange30d,
+        percentChange60d = this.percentChange60d,
+        percentChange7d = this.percentChange7d,
+        percentChange90d = this.percentChange90d,
+        price = this.price,
+        selfReportedMarketCap = this.selfReportedMarketCap,
+        turnover = this.turnover,
+        tvl = this.tvl,
+        volume24h = this.volume24h,
+        volume30d = this.volume30d,
+        volume7d = this.volume7d,
+        ytdPriceChangePercentage = this.ytdPriceChangePercentage
+)
