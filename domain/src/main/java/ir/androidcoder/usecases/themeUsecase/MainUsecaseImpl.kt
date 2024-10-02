efc51a56aaac4b71292dfd15f379e5c7cc69dee5
Mohammad Hosein <m.hosein.developer@gmail.com>
2024-10-02 06:16:49 -1200

    Create And Setup Main Usecase

---
+++ domain/src/main/java/ir/androidcoder/usecases/themeUsecase/MainUsecaseImpl.kt
@@ -0,0 +1,31 @@
+package ir.androidcoder.usecases.themeUsecase
+
+import ir.androidcoder.entities.CryptoCurrencyEntity
+import ir.androidcoder.entities.PriceEntity
+import ir.androidcoder.repositories.mainRepo.MainRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class MainUsecaseImpl(private val repository : MainRepository) : MainUsecase  {
+
+    override val getCryptoList: Flow<List<CryptoCurrencyEntity>> = repository.getCryptoList.map { it }
+
+    override val getCryptoListFromDb: Flow<List<CryptoCurrencyEntity>> = repository.getCryptoListFromDb.map { it }
+
+    override suspend fun getCryptoByIdFromDb(id: Int): CryptoCurrencyEntity = repository.getCryptoByIdFromDb(id)
+
+    override val getDollarPrice: Flow<PriceEntity> = repository.getDollarPrice.map { it }
+
+    override val getBookmarkList: Flow<List<CryptoCurrencyEntity>> = repository.getBookmarkList.map { it }
+
+
+    override suspend fun insertBookmark(data: CryptoCurrencyEntity) {
+        repository.insertBookmark(data)
+    }
+
+    override suspend fun deleteBookmark(id: Int) {
+        repository.deleteBookmark(id)
+    }
+
+
+}
